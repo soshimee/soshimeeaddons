183d85c860a2c4240357bb117118ea0c51b204e2
whhhhhhh <yejungkim0418@gmail.com>
2024-06-22 21:20:48 +0900

    progress for melody alert

--- features/melodyAlert/index.js
+++ features/melodyAlert/index.js
@@ -1,17 +1,35 @@
 import Settings from "../../config";
 import packetOpenWindow from "../../events/packetOpenWindow";
+import packetSetSlot from "../../events/packetSetSlot";
+import closeWindow from "../../events/closeWindow";
 
-function listener(title) {
+function openWindowListener(title) {
 	if (title !== "Click the button on time!") return;
-	ChatLib.command("party chat " + Settings.melodyAlertMessage);
+	closeWindow.addListener(closeWindowListener);
+	packetSetSlot.addListener(setSlotListener);
+}
+
+function setSlotListener(itemStack, slot) {
+	if (itemStack === null) return;
+	const slots = [16, 25, 34, 43];
+	const index = slots.indexOf(slot);
+	if (index === -1) return;
+	const item = new Item(itemStack);
+	if (item.getID() !== 159 || item.getMetadata() !== 5) return;
+	ChatLib.command("party chat " + Settings.melodyAlertMessage + " " + index + "/4");
+}
+
+function closeWindowListener() {
+	closeWindow.removeListener(closeWindowListener);
+	packetSetSlot.removeListener(setSlotListener);
 }
 
 export function enable() {
-	packetOpenWindow.addListener(listener);
+	packetOpenWindow.addListener(openWindowListener);
 }
 
 export function disable() {
-	packetOpenWindow.removeListener(listener);
+	packetOpenWindow.removeListener(openWindowListener);
 }
 
 export default { enable, disable };
