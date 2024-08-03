9265c6e104b70aa57ef9cc8529332f24867055f1
freebonsai <118639445+freebonsai@users.noreply.github.com>
2024-08-03 17:00:28 +0200

    Cleanup

--- features/shadowAssassinAlert/index.js
+++ features/shadowAssassinAlert/index.js
@@ -1,13 +1,13 @@
 import Dungeon from "../../../BloomCore/dungeons/Dungeon";
 import packetWorldBorder from "../../events/packetWorldBorder";
-import chat from "../../utils/chat";
+import { chat } from "../../utils/chat";
 
 function listener(worldborder) {
 	if (!Dungeon.inDungeon) return;
 	if (worldborder.func_177741_h() !== 1) return;
 	Client.showTitle(" ", "§aShadow Assassin!", 0, 30, 0);
 	Client.showTitle(" ", "§aShadow Assassin!", 0, 30, 0);
-	chat.chat("§aShadow Assassin!");
+	chat("§aShadow Assassin!");
 	World.playSound("mob.blaze.hit", 1, 1);
 	World.playSound("mob.blaze.hit", 1, 1);
 	World.playSound("mob.blaze.hit", 1, 1);
